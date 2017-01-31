39e9d2de040c04ddb914668d11430eff994d1d0d
elizafischer <efischer@fwparker.org>
2017-01-30 19:21:16 -0600

    Done

--- ch00_problem_set(LOOPS).py
+++ ch00_problem_set(LOOPS).py
@@ -30,7 +30,7 @@ print("")
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
 print("Problem #2")
-'''
+
 number = random.randrange(0,1001)
 #print(number)
 
@@ -51,32 +51,34 @@ if user < number:
 if user == number:
     print("You guessed the number correctly!")
     print("")
-'''
+
 # PROBLEM 3 (Dice Hi-Low - 6pts) ****
 # You roll five six-sided dice, one by one.
 # How big is the probability that the value of each die is greater than or equal to the value of the previous die that you rolled?
 # For example, the sequence “1, 1, 4, 4, 6” is a success, but “1, 1, 4, 3, 6” is not.
 # Determine the probability of success using a simulation of a large number of trials.
 print("Problem #3")
-
+'''
 sucesses = 0
-total = 1000
+total = 10
 failed = 0
-for i in range(1000):
+for i in range(10):
     roll = []
     for o in range(1, 6):
-        dice_roll = random.randrange(1,7)
+        dice_roll = (random.randrange(1,7))
         roll.append(dice_roll)
-        if (int(roll[0]) <= int(roll[1]) and (int(roll[1]) <= int(roll[2])) and (int(roll[2]) <= int(roll[3])) and (
-            int(roll[3]) <= int(roll[4])) and (int(roll[4]) <= int(roll[5])):
+        #print(roll)
+        if int(roll[0]) <= int(roll[1] and int(roll[1]) <= int(roll[2]) and int(roll[2]) <= int(roll[3]) and int(roll[3]) <= int(roll[4]) and int(roll[4]) <= int(roll[5])):
             sucesses += 1
         else:
             failed += 1
 
 print("The probability that the value of each roll is greater than or equal to the previous is:", ((sucesses) / (sucesses + failed)) * 100, "percent")
+# I have been working at this for a while and am not completely sure why it does not work
+'''
 
 print("")
-# PROBLEM 4 (Number Puzzler - 6pts) *****
+# PROBLEM 4 (Number Puzzler - 6pts)
 # A, B, C, and D are all different digits.
 # The number DCBA is equal to 4 times the number ABCD.
 # What are the digits?
@@ -88,5 +90,5 @@ for a in range(10):
     for b in range(10):
         for c in range(10):
             for d in range(10):
-                if ((int(d) * int(c) * int(b) * int(a)) == 4 * (int(a) * int(b) * int(c) * int(d))) and a != 0 and d != 0:
-                    print ("A = " + str(a) + " B = " + str(b) + " C = " + str(c) + " and D = " + str(d))
+                if int(str(d) + str(c) + str(b) + str(a)) == 4 * int(str(a) + str(b) + str(c) + str(d)) and a!= 0 and d != 0:
+                    print("A = " + str(a) + ", B = " + str(b) + ", C = " + str(c) + ", and D = " + str(d))
